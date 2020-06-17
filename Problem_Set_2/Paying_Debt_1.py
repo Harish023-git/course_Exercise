2a62a6642ad9f5f2631b384d35a1cbef7e51b323
Harish K M <56727448+Harish023-git@users.noreply.github.com>
2020-06-17 10:48:01 +0530

    Update Paying_Debt_1.py

--- Problem_Set_2/Paying_Debt_1.py
+++ Problem_Set_2/Paying_Debt_1.py
@@ -1,12 +1,11 @@
-"""Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
+""" Problem - 1: 
+Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
 
 The following variables contain values as described below:
 
-balance - the outstanding balance on the credit card
-
-annualInterestRate - annual interest rate as a decimal
-
-monthlyPaymentRate - minimum monthly payment rate as a decimal
+1) balance - the outstanding balance on the credit card
+2) annualInterestRate - annual interest rate as a decimal
+3) monthlyPaymentRate - minimum monthly payment rate as a decimal
 
 Monthly interest rate= (Annual interest rate) / 12.0
 Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
