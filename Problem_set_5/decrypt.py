800776588ba5a5b8fc55552ec5738839d7570bde
Harish K M <56727448+Harish023-git@users.noreply.github.com>
2020-07-21 16:46:24 +0530

    Create decrypt.py

---
+++ Problem_set_5/decrypt.py
@@ -0,0 +1,9 @@
+"""
+Now that you have all the pieces to the puzzle, please use them to decode the file story.txt. The file ps6.py contains a helper function get_story_string() that 
+returns the encrypted version of the story as a string. Create a CiphertextMessage object using the story string and use decrypt_message to return the appropriate 
+shift value and unencrypted story string.
+"""
+
+def decrypt_story():
+    message = CiphertextMessage(get_story_string())
+    return message.decrypt_message()
